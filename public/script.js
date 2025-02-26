3bb6a92b875e57ff87219312e5681659c9899d19
= <toth.levente.21i@egressy.info>
2025-02-26 13:00:47 +0100

    fix1

--- public/script.js
+++ public/script.js
@@ -21,20 +21,35 @@ function login(){
     }
 }
 
+const emailInput = document.getElementById('email')
+const usernameInput = document.getElementById('username')
+const passwordInput = document.getElementById('password')
+
 function register(){
-    const registerreq = new XMLHttpRequest()
-    registerreq.open('post', '/registerRequest')
-    registerreq.setRequestHeader('Content-Type', 'Application/json')
-    registerreq.send(JSON.stringify({
-        'remail': email.value,
-        'rusername': username.value,
-        'rpassword': password.value
-    }))
-    registerreq.onreadystatechange = ()=>{
-        if(registerreq.readyState == 4){
-            const result = JSON.parse(registerreq.response)
-            console.log(result.message)
-            alert(result.message)
+    if(emailInput.value.length >= 10 && usernameInput.value.length > 3 && passwordInput.value.length >= 8){
+        const registerreq = new XMLHttpRequest()
+        registerreq.open('post', '/registerRequest')
+        registerreq.setRequestHeader('Content-Type', 'Application/json')
+        registerreq.send(JSON.stringify({
+            'remail': email.value,
+            'rusername': username.value,
+            'rpassword': password.value
+        }))
+        registerreq.onreadystatechange = ()=>{
+            if(registerreq.readyState == 4){
+                const result = JSON.parse(registerreq.response)
+                console.log(result.message)
+                alert(result.message)
+            }
         }
+    }
+    else if(emailInput.value.length < 10){
+        alert('Túl rövid az email cím, legalább 10 karakter legyen!')
+    }
+    else if(usernameInput.value.length <= 3){
+        alert('A felhasználónév túl rövid!')
+    }
+    else if(passwordInput.value.length < 8){
+        alert('A jelszó legalább 8 karakter legyen!')
     }
 }
